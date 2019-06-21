2ab30b35b166560d0c5b7807f553d6b3441e561d
Junior <junior.arodriguez@icloud.com>
2019-06-20 19:15:15 -0500

    added exact path to Home / and added this.props to grab id passed to the URL

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>
-        <Route path="/" component={MovieList} />
+        <Route exact path="/" component={MovieList} />
         <Route path="/movies/:id" component={Movie} />
         </div>
       </div>
